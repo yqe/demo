5365eb8306048b5fd505a8ba212afb6131b4cd06
yp <1013799318@qq.com>
2015-11-23 12:55:21 +0800

    桩和驱动还有Mock
    被移动到新建的包中

---
+++ client/stubanddriver/mock/Generatedocutester1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package mock;
+
+import org.junit.Test;
+
+import documentbl.Transdocu;
+
+/**
+ * @author jjlb
+ *
+ */
+public class Generatedocutester1 {
+
+//	@Test
+//	public void test() {
+//		MockInfocollector Infocollector1=new MockInfocollector(20150910,"11110","南京","北京","张三","李四","114500",55.0);
+//		Transdocu transdocu=new Transdocu();
+//		transdocu.generate(Infocollector1);
+//		assertEquals(true,transdocu.ifadd());
+//	}
+
+}
