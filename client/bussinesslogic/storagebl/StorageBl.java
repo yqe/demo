5e02107c0ea6a66f4eb77b0d242ad1a59d70ad5e
Vapid33 <zwqing14@software,nju.edu.cn>
2015-11-28 20:04:32 +0800

    假数据。。

--- client/bussinesslogic/storagebl/StorageBl.java
+++ client/bussinesslogic/storagebl/StorageBl.java
@@ -7,73 +7,90 @@ import po.OutStorageList;
 import po.StorageList;
 import po.StoragePO;
 import storageblService.StorageBlService;
-import storagedataService.StorageDataService;
 
 
 public class StorageBl implements StorageBlService{
 	
 	public void InStorageInput(StorageList slt) {
-		try {
-			Socket insto=new Socket("localhost",8889);
-			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
-			oos.writeUTF("Storage");
-			oos.writeUTF("InStorageInput");
-			oos.writeObject(slt);
-			oos.flush();
-			oos.close();
-			insto.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println(slt.getSlist().get(0).getDestination());
+		System.out.println("StorageBl.InStorageInputSuccess");
+//		try {
+//			Socket insto=new Socket("localhost",8889);
+//			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
+//			oos.writeUTF("Storage");
+//			oos.writeUTF("InStorageInput");
+//			oos.writeObject(slt);
+//			oos.flush();
+//			oos.close();
+//			insto.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
-
+//
 	public void InStorageDetele(int rank) {
-		
+		System.out.println("Storagebl.InStorageDeteleSuccess");
 	}
 
 
 	public void OutStorageInput(OutStorageList oslt) {
-		try {
-			Socket insto=new Socket("localhost",8889);
-			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
-			oos.writeUTF("OutStorageInput");
-			oos.writeObject(oslt);
-			oos.flush();
-			oos.close();
-			insto.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.print(oslt.getSlist().get(0).getDestination());
+		System.out.println("StorageBl.OutStorageInputSuccess");
+//		try {
+//			Socket insto=new Socket("localhost",8889);
+//			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
+//			oos.writeUTF("OutStorageInput");
+//			oos.writeObject(oslt);
+//			oos.flush();
+//			oos.close();
+//			insto.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 
 	public void OutStorageDelete(int rank) {
+		System.out.println("StorageBl.OutStorageDeleteSuccess");
 	}
 
 
-	public void StorageCheck(StorageList slt) {
+	public StorageList StorageCheck() {
+		StoragePO a = new StoragePO("nanjing","210046","2015.10.11","beijing","01",23,12,23);
+      
+		StorageList n=new StorageList();
+        n.addStorageLineItem(a);;
+		
+       
+		System.out.println("StorageBl.StorageCheckSuccess");
+		return n;
+		
 		
 	}
 
 
-	public void StorageSee(String info) {
+	public String[] StorageSee() {
+		String[] str={"100","100","100"};
+		return str;
 	}
 
 
 	public void StorageUpdate(StoragePO svo) {
-		try {
-			Socket insto=new Socket("localhost",8888);
-			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
-			oos.writeUTF("StorageUpdate");
-			oos.writeObject(svo);
-			oos.flush();
-			oos.close();
-			insto.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+      System.out.println("StorageBl.StorageUpdateSuccess");
+		//		try {
+//			Socket insto=new Socket("localhost",8888);
+//			ObjectOutputStream oos=new ObjectOutputStream(insto.getOutputStream());
+//			oos.writeUTF("StorageUpdate");
+//			oos.writeObject(svo);
+//			oos.flush();
+//			oos.close();
+//			insto.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
+
 
 
 }
