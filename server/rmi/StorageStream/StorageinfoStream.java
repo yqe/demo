ad5de3cc7b265c1c47cdd6d5cd741d3298f7b202
zXingchu <2649258169@qq.com>
2015-12-01 21:06:16 +0800

    rmiStorage更新

--- server/rmi/StorageStream/StorageinfoStream.java
+++ server/rmi/StorageStream/StorageinfoStream.java
@@ -1,20 +1,52 @@
 package StorageStream;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import po.StorageList;
+import po.InputStorageList;
+import po.OutStorageList;
 import storagedata.StorageData;
 
-public class StorageinfoStream {
-	StorageData sd=new StorageData();
-	public StorageList InStorageInfoGet(ObjectInputStream ois){
+public class StorageInfoStream {
+
+	public void JudgeCmd(ObjectInputStream ois, ObjectOutputStream oos) {
 		try {
-			StorageList slt=(StorageList)ois.readObject();
-			sd.StorageDataAdd(slt);
+			if (ois.readUTF().equals("InStoragePO"))
+				InStorageInfoGet(ois,oos);
+			if(ois.readUTF().equals("OutStoragePO"))
+				OutStorageInfoGet(ois,oos);
+			if(ois.readUTF().equals("CheckStorage"))
+				StorageCheckInfoGet(ois,oos);
+			if(ois.readUTF().equals("SeeStorage"))
+				StorageSeeInfoGet(ois,oos);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void InStorageInfoGet(ObjectInputStream ois, ObjectOutputStream oos) {
+		try {
+			InputStorageList slt = (InputStorageList) ois.readObject();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		return null;
+		}
+	}
+
+	public void OutStorageInfoGet(ObjectInputStream ois, ObjectOutputStream oos) {
+		try {
+			OutStorageList slt = (OutStorageList) ois.readObject();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void StorageCheckInfoGet(ObjectInputStream ois, ObjectOutputStream oos) {
+		
+	}
+	public void StorageSeeInfoGet(ObjectInputStream ois, ObjectOutputStream oos) {
 		
 	}
 }
