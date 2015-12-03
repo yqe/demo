96a3537468d13781c6c897bbe50f8e2f5bc90664
yp <1013799318@qq.com>
2015-12-03 20:39:17 +0800

    加了营业厅编号

--- server/permanent/po/BussinessArrivalDocuPO.java
+++ server/permanent/po/BussinessArrivalDocuPO.java
@@ -2,15 +2,20 @@ package po;
 
 public class BussinessArrivalDocuPO {
 	//营业厅到达单
+	private String bussinessID;//营业厅编号
 	private String arrivaltime;//到达日期
 	private String transferID;//中转编号
 	private String destination;//目的地
 	private String state;//到达状态
-	public BussinessArrivalDocuPO(String atime,String ID,String des,String state){
+	public BussinessArrivalDocuPO(String atime,String ID,String des,String state,String bussinessID){
+		this.bussinessID=bussinessID;
 		this.arrivaltime=atime;
 		this.transferID=ID;
 		this.destination=des;
 		this.state=state;
+	}
+	public String getBussinessID() {
+		return bussinessID;
 	}
 	public String getArrivaltime() {
 		return arrivaltime;
