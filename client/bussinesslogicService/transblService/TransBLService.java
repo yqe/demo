2bd9dbe47ca752add2a19ba3fc53a4e85040be35
zXingchu <2649258169@qq.com>
2015-12-06 16:24:18 +0800

    接口调整

--- client/bussinesslogicService/transblService/TransBLService.java
+++ client/bussinesslogicService/transblService/TransBLService.java
@@ -9,9 +9,32 @@ package transblService;
 
 import java.rmi.RemoteException;
 
+import po.VehicleMaintanceInfoPO;
+
 public interface TransBlService {
 	public String TransMaintenance(String info);// 更改车辆维护状态
 
 	public String Transgetfee(String type, String depatureplace, String destination);// 计算并显示交通运费
-
+	
+	/**
+	 * 得到车辆维护信息
+	 * 
+	 * @param String
+	 *            vehicleID
+	 * @return VehicleMaintanceInfoPO
+	 * @exception @author
+	 *                zxc
+	 */
+	public VehicleMaintanceInfoPO GetVehicleInfoPO(String vehicleID);
+	
+	/**
+	 * 车辆信息改变
+	 * 
+	 * @param String
+	 *            vehicleID
+	 * @return VehicleMaintanceInfoPO
+	 * @exception @author
+	 *                zxc
+	 */
+	public boolean ChangeVehicleInfoPO(VehicleMaintanceInfoPO vpo);
 }
