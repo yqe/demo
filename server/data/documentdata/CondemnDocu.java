01579215c285c54f5db4dbe642a831f0b1a47a63
yp <1013799318@qq.com>
2015-12-06 22:08:24 +0800

    审批单据

---
+++ server/data/documentdata/CondemnDocu.java
@@ -0,0 +1,5 @@
+package documentdata;
+
+public class CondemnDocu {
+
+}
---
+++ server/permanent/po/CondemnDocuPO.java
@@ -0,0 +1,30 @@
+package po;
+
+import java.io.Serializable;
+
+/**
+ * @author jjlb
+ *审批单据
+ */
+public class CondemnDocuPO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String type;//单据类型o
+	private String ID;//单据编号
+	private String state;//审批状态
+	public CondemnDocuPO(String type,String ID,String state){
+		this.type=type;
+		this.ID=ID;
+		this.state=state;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getID() {
+		return ID;
+	}
+	public String getState() {
+		return state;
+	}
+	
+	
+}
