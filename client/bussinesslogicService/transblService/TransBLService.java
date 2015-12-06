7a7183e2de42ae4cb4518d7297e55fdec844a2ce
zXingchu <2649258169@qq.com>
2015-12-06 16:22:46 +0800

    更正大小写

--- client/bussinesslogicService/transblService/TransBLService.java
+++ client/bussinesslogicService/transblService/TransBLService.java
@@ -6,9 +6,12 @@
  *
  */
 package transblService;
+
 import java.rmi.RemoteException;
-public interface TransBLService{
-	public String TransMaintenance(String info);//更改车辆维护状态
-	public String Transgetfee(String type,String depatureplace,String destination);//计算并显示交通运费
+
+public interface TransBlService {
+	public String TransMaintenance(String info);// 更改车辆维护状态
+
+	public String Transgetfee(String type, String depatureplace, String destination);// 计算并显示交通运费
 
 }
