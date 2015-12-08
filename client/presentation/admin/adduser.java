fdc31b7e911ae61f4a52efb52453db927da6e738
yqe <541123989@qq.com>
2015-12-08 20:06:32 +0800

    adduser

--- client/presentation/admin/adduser.java
+++ client/presentation/admin/adduser.java
@@ -35,36 +35,36 @@ public class adduser {
 
 		JLabel l4 = new JLabel("姓名:");
 		JLabel l5 = new JLabel("职位:");
+		
+		JLabel l6 = new JLabel("设置密码:");
 
 		l3.setFont(new Font("", Font.PLAIN, b2size));
 		l4.setFont(new Font("", Font.PLAIN, b2size));
 		l5.setFont(new Font("", Font.PLAIN, b2size));
-
+		l6.setFont(new Font("", Font.PLAIN, b2size));
+		
 		final JTextField t1 = new JTextField();
 		final JTextField t2 = new JTextField();
+		final JTextField t3 = new JTextField();
 		final String[] jobs = { "营业厅业务员", "快递员", "中转中心业务员", "中转中心库存管理人员", "总经理", "财务人员", "管理员" };
 
 		final JComboBox job = new JComboBox(jobs);
 
-//		t2.setOpaque(false);
-//		t3.setOpaque(false);
-//		t2.setEditable(false);
-//		t3.setEditable(false);
+
 
 		JButton b5 = new JButton("确认新建");
 		b5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// PO传数据
-//				UserBl userbl = new UserBl();
-//				UserInfoPO userpotemp = userbl.CheckUserInfoPO(t1.getText());
-//				boolean IsOk=userbl.cancellation(userpotemp);
+		
 			boolean idisempty=t1.getText().equals("");
 				boolean nameisempty=t2.getText().equals("");
-				boolean isempty= idisempty||nameisempty;
+				boolean passwordisempty=t3.getText().equals("");
+				boolean isempty= idisempty||nameisempty||passwordisempty;
 				
+				UserInfoPO user=new UserInfoPO(t1.getText(), t3.getText(), t2.getText(), job.getSelectedItem().toString());
+				UserBl userbl=new UserBl();		
 				
-				
-				if(!isempty)
+				if(!isempty&&userbl.AddUser(user))
 				JOptionPane.showMessageDialog(null, "新建成功!");
 				else
 				JOptionPane.showMessageDialog(null, "新建失败，请确认信息填写完整!");
@@ -80,9 +80,11 @@ public class adduser {
 		p1.add(l3);
 		p1.add(l4);
 		p1.add(l5);
+		p1.add(l6);
 
 		p1.add(t1);
 		p1.add(t2);
+		p1.add(t3);
 		p1.add(job);
 
 		p1.add(b5);
@@ -96,14 +98,16 @@ public class adduser {
 
 		l2.setBounds(50, b1yloc, 150, 30);
 
-		l3.setBounds(150, 180, 150, 30);
+		l3.setBounds(150, 150, 150, 30);
 
-		l4.setBounds(150, 240, 150, 30);
+		l4.setBounds(150, 250, 150, 30);
 		l5.setBounds(150, 300, 150, 30);
+		l6.setBounds(150, 200, 150, 30);
 
-		t1.setBounds(300, 180, 180, 30);
-		t2.setBounds(300, 240, 180, 30);
-		job.setBounds(300, 300, 180, 30);
+		t1.setBounds(300, 150, 180, 30);
+        t2.setBounds(300, 250, 180, 30);
+        t3.setBounds(300, 200, 180, 30);
+        job.setBounds(300, 300, 180, 30);
 
 		b5.setBounds(b4xloc, b4yloc, 150, 30);
 		return p1;
