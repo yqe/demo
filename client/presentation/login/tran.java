f8e63551583e5a897bb9f433a74a91ffd68872f3
yqe <541123989@qq.com>
2015-12-09 22:09:22 +0800

    tran

--- client/presentation/login/tran.java
+++ client/presentation/login/tran.java
@@ -1,8 +1,8 @@
 package login;
 
-public class tran {
+public class Tran {
 	
-	public static String tran(String str) {
+	public static String Tran(String str) {
 		str = str.replace('年', '-');
 		str = str.replace('月', '-');
 		str = str.substring(0, str.length()-1);
