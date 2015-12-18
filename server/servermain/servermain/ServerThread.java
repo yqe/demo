cd5515033944403bef28ec47f5c233c172d64450
zXingchu <2649258169@qq.com>
2015-12-18 20:48:17 +0800

    小修

--- server/servermain/servermain/ServerThread.java
+++ server/servermain/servermain/ServerThread.java
@@ -47,89 +47,75 @@ public class ServerThread implements Runnable {
 		}
 	}
 
-	private class Receiver implements Runnable {
+	private class Receiver {
 		Socket socket;
 		ObjectInputStream ois = null;
 		ObjectOutputStream oos = null;
 
 		Receiver(Socket socket) {
 			this.socket = socket;
-			try {
-				oos = new ObjectOutputStream(socket.getOutputStream());
-				ois = new ObjectInputStream(socket.getInputStream());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		public void close() {
-			try {
-				oos.close();
-				ois.close();
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 
 		public void login() {
 			String logininfo = null;
-			int i = 0;
-			while (logininfo == null) {
+			try {
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				ois = new ObjectInputStream(socket.getInputStream());
+				while (((String) ois.readObject()).equals("login")) {
+					logininfo = new AdminInfoStream().Login(ois, oos);
+					if (!logininfo.equals("NoAccount") && !logininfo.equals("PasswordError")) {
+						dealcmd(ois, oos);
+						break;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void dealcmd(ObjectInputStream ois, ObjectOutputStream oos) {
+			while (true) {
 				try {
-					i++;
-					if (((String) ois.readObject()).equals("login")) {
-						logininfo = new AdminInfoStream().Login(ois, oos);
-						if (!logininfo.equals("NoAccount") && !logininfo.equals("PasswordError")) {
-							Thread t = new Thread(this);
-							t.start();
-						} else
-							logininfo = null;
+					switch (ois.readUTF()) {
+					case "Storage":
+						new StorageInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "Admin":
+						new AdminInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "Courier":
+						new CourierInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "Finance":
+						new FinanceInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "HallClerk":
+						new HallClerkInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "Manager":
+						new ManagerInfoStream().JudgeCmd(ois, oos);
+						break;
+					case "Transit":
+						new TransitInfoStream().JudgeCmd(ois, oos);
+						break;
+					default:
+						close();
+						break;
 					}
-					else{
-						if(((String) ois.readObject()).equals("GetRoute")){
-							new CourierInfoStream().GetRoute(ois,oos);
-							logininfo="route";
-						}
-					}
-					System.out.println(logininfo + i);
 				} catch (Exception e) {
-					close();
+					e.printStackTrace();
 				}
 			}
 		}
 
-		public void run() {
+		private void close() {
 			try {
-				switch (ois.readUTF()) {
-				case "Storage":
-					new StorageInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "Admin":
-					new AdminInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "Courier":
-					new CourierInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "Finance":
-					new FinanceInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "HallClerk":
-					new HallClerkInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "Manager":
-					new ManagerInfoStream().JudgeCmd(ois, oos);
-					break;
-				case "Transit":
-					new TransitInfoStream().JudgeCmd(ois, oos);
-					break;
-				default:
-					close();
-					break;
-				}
-			} catch (Exception e) {
-				close();
+				ois.close();
+				oos.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
