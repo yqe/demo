5d9ae38e7889df40b5dc758c33df05502af9e29e
zXingchu <2649258169@qq.com>
2015-12-25 08:53:26 +0800

    增删em

--- client/presentation/manager/addemployee.java
+++ client/presentation/manager/addemployee.java
@@ -36,7 +36,7 @@ public class addemployee {
 	private ObjectOutputStream oos;
 	private ObjectInputStream ois;
 	private EmploeePO emPO;
-	JPanel p1 = new JPanel(){
+	JPanel p1 = new JPanel() {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.drawImage(background.getImage(), 0, 0, null);
@@ -44,46 +44,17 @@ public class addemployee {
 	};
 
 	public addemployee(ObjectOutputStream oos, ObjectInputStream ois, EmploeePO emPO) {
-		this.oos=oos;
-		this.ois=ois;
-		this.emPO=emPO;
+		this.oos = oos;
+		this.ois = ois;
+		this.emPO = emPO;
 	}
 
 	public JPanel Panel() throws IOException {
-		 new ImageGet();
-	        Image bgp=ImageGet.getImageByState("addemployee");
+		new ImageGet();
+		Image bgp = ImageGet.getImageByState("addemployee");
 		background = new ImageIcon(bgp);
-	
-		p1.setBounds(0, 0,1029, 840);
-		JLabel l1 = new JLabel("快递物流系统");
-		int b1size = 30;
-		l1.setFont(new Font("快递物流系统", Font.PLAIN, b1size));
-		JLabel l2 = new JLabel("—>添加员工");
-		int b2size = 16;
-		l2.setFont(new Font("—>主页", Font.PLAIN, b2size));
-		JLabel l3 = new JLabel("ID:");
 
-		JLabel l4 = new JLabel("姓名:");
-		JLabel l5 = new JLabel("职位:");
-		JLabel l6 = new JLabel("年龄:");
-		JLabel l7 = new JLabel("手机:");
-		JLabel l8 = new JLabel("薪水:");
-		JLabel l9 = new JLabel("身份证:");
-		JLabel l10 = new JLabel("地址:");
-		JLabel l11 = new JLabel("性别:");
-		JLabel l12 = new JLabel("地区:");
-		JLabel posIDlabel = new JLabel("机构编号:");
-		l3.setFont(new Font("", Font.PLAIN, b2size));
-		l4.setFont(new Font("", Font.PLAIN, b2size));
-		l5.setFont(new Font("", Font.PLAIN, b2size));
-		l6.setFont(new Font("", Font.PLAIN, b2size));
-		l7.setFont(new Font("", Font.PLAIN, b2size));
-		l8.setFont(new Font("", Font.PLAIN, b2size));
-		l9.setFont(new Font("", Font.PLAIN, b2size));
-		l10.setFont(new Font("", Font.PLAIN, b2size));
-		l11.setFont(new Font("", Font.PLAIN, b2size));
-		l12.setFont(new Font("", Font.PLAIN, b2size));
-		posIDlabel.setFont(new Font("", Font.PLAIN, b2size));
+		p1.setBounds(0, 0, 1029, 840);
 
 		final JTextField id = new JTextField();
 		final JTextField name = new JTextField();
@@ -109,7 +80,7 @@ public class addemployee {
 		address.setBorder(BorderFactory.createEmptyBorder());
 		posidtext.setOpaque(false);
 		posidtext.setBorder(BorderFactory.createEmptyBorder());
-		
+
 		final String[] jobs = { "营业厅业务员", "快递员", "中转中心业务员", "中转中心库存管理人员", "总经理", "财务人员", "管理员" };
 
 		final JComboBox job = new JComboBox(jobs);
@@ -126,15 +97,10 @@ public class addemployee {
 		bg.add(jb1);
 		bg.add(jb2);
 
-		// t2.setOpaque(false);
-		// t3.setOpaque(false);
-		// t2.setEditable(false);
-		// t3.setEditable(false);
-
-		JButton b5 = new JButton("确认添加");
+		JButton b5 = new JButton("");
 		b5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-	
+
 				boolean idisempty = id.getText().equals("");
 				boolean nameisempty = name.getText().equals("");
 				boolean ageisempty = age.getText().equals("");
@@ -143,44 +109,29 @@ public class addemployee {
 				boolean identityisempty = identity.getText().equals("");
 				boolean addressisempty = address.getText().equals("");
 				boolean posidtextisempty = posidtext.getText().equals("");
-				
-				
-				boolean isempty = idisempty || nameisempty||ageisempty||telisempty||salaryisempty||identityisempty
-						||addressisempty||posidtextisempty;
 
-				if (!isempty ){
+				boolean isempty = idisempty || nameisempty || ageisempty || telisempty || salaryisempty
+						|| identityisempty || addressisempty || posidtextisempty;
+
+				if (!isempty) {
 					EmploeePO epo = new EmploeePO(job.getSelectedItem().toString(), id.getText(), name.getText(),
 							Integer.parseInt(salary.getText()), bg.getElements().toString(),
 							Integer.parseInt(age.getText()), tel.getText(), identity.getText(), address.getText(),
 							place.getSelectedItem().toString(), posidtext.getText());
-					EmploeeBl ac = new EmploeeBl(oos,ois);
+					EmploeeBl ac = new EmploeeBl(oos, ois);
 					boolean isOk = ac.AddEmpInfo(epo);
-					if(isOk){
-					   JOptionPane.showMessageDialog(null, "添加成功!");
-					   }
+					if (isOk) {
+						JOptionPane.showMessageDialog(null, "添加成功!");
 					}
-				else{
+				} else {
 					JOptionPane.showMessageDialog(null, "添加失败，请确认信息填写完整!");
-				   }
-			} 
+				}
+			}
 
 		});
 
 		p1.setOpaque(false);
 		p1.setLayout(null);
-		p1.add(l1);
-		p1.add(l2);
-		p1.add(l3);
-		p1.add(l4);
-		p1.add(l5);
-		p1.add(l6);
-		p1.add(l7);
-		p1.add(l8);
-		p1.add(l9);
-		p1.add(l10);
-		p1.add(l11);
-		p1.add(l12);
-		p1.add(posIDlabel);
 
 		p1.add(id);
 		p1.add(name);
@@ -197,41 +148,24 @@ public class addemployee {
 
 		p1.add(b5);
 
-		int b1xloc = p1.getWidth() * 7 / 12 + 20, b1xsize = p1.getWidth() * 4 / 25 - 15;
-		int b1yloc = p1.getHeight() * 2 / 15;
-		int b4xloc = p1.getWidth() * 2 / 5;
-		int b4yloc = 600, b4ysize = p1.getHeight() * 1 / 5 + 10;
-		int labelw = 150;
-		int labelh = 30;
-		l1.setBounds(220, -20, 180, 80);
-		l2.setBounds(50, 50, labelw, labelh);
-		l3.setBounds(150, 100, labelw, labelh);
-		l4.setBounds(150, 150, labelw, labelh);
-		l5.setBounds(150, 300, labelw, labelh);
-		l6.setBounds(150, 350, labelw, labelh);
-		l7.setBounds(150, 400, labelw, labelh);
-		l8.setBounds(150, 450, labelw, labelh);
-		l9.setBounds(150, 500, labelw, labelh);
-		l10.setBounds(150, 550, labelw, labelh);
-		l11.setBounds(150, 600, labelw, labelh);
-		l12.setBounds(150, 200, labelw, labelh);
-		posIDlabel.setBounds(150, 250, labelw, labelh);
-		int textw = 150;
-		int texth = 30;
-		id.setBounds(300, 100, textw, texth);
-		name.setBounds(300, 150, textw, texth);
-		job.setBounds(300, 300, textw, texth);
-		age.setBounds(300, 350, textw, texth);
-		tel.setBounds(300, 400, textw, texth);
-		salary.setBounds(300, 450, textw, texth);
-		identity.setBounds(300, 500, textw, texth);
-		address.setBounds(300, 550, textw, texth);
-		jb1.setBounds(300, 600, 50, 30);
-		jb2.setBounds(350, 600, 50, 30);
-		place.setBounds(300, 200, 100, 30);
-		posidtext.setBounds(300, 250, textw, texth);
+		int textw = 142;
+		int texth = 29;
+		id.setBounds(182, 232, textw, texth);
+		name.setBounds(646, 162, 94, 28);
+		job.setBounds(184, 450, textw, texth);
+		age.setBounds(646, 232, 94, 28);
+		tel.setBounds(646, 306, textw, texth);
+		salary.setBounds(184, 534, textw, texth);
+		identity.setBounds(646, 384, textw, texth);
+		address.setBounds(646, 457, textw, texth);
+		jb1.setBounds(651, 536, 44, 28);
+		jb2.setBounds(651+47+6, 536, 44, 28);
+		place.setBounds(182, 306, textw, texth);
+		posidtext.setBounds(182, 378, textw, texth);
 
-		b5.setBounds(b4xloc, b4yloc + 30, 150, 30);
+		b5.setBounds(603, 652, 288, 63);
+		b5.setContentAreaFilled(false);
+		b5.setBorder(BorderFactory.createEmptyBorder());
 		return p1;
 
 	}
--- client/presentation/manager/deleteemployee.java
+++ client/presentation/manager/deleteemployee.java
@@ -36,7 +36,7 @@ public class deleteemployee {
 	private ObjectOutputStream oos;
 	private ObjectInputStream ois;
 	private EmploeePO emPO;
-	JPanel p1 = new JPanel(){
+	JPanel p1 = new JPanel() {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.drawImage(background.getImage(), 0, 0, null);
@@ -44,46 +44,18 @@ public class deleteemployee {
 	};
 
 	public deleteemployee(ObjectOutputStream oos, ObjectInputStream ois, EmploeePO emPO) {
-		this.oos=oos;
-		this.ois=ois;
-		this.emPO=emPO;
+		this.oos = oos;
+		this.ois = ois;
+		this.emPO = emPO;
 	}
 
 	public JPanel Panel() throws IOException {
-		 new ImageGet();
-	        Image bgp=ImageGet.getImageByState("deleteemployee");
+		new ImageGet();
+		Image bgp = ImageGet.getImageByState("deleteemployee");
 		background = new ImageIcon(bgp);
-		
-		p1.setBounds(0, 0,1029, 840);
-		JLabel l1 = new JLabel("快递物流系统");
-		int b1size = 30;
-		l1.setFont(new Font("快递物流系统", Font.PLAIN, b1size));
-		JLabel l2 = new JLabel("—>删除员工");
-		int b2size = 16;
-		l2.setFont(new Font("—>主页", Font.PLAIN, b2size));
-		JLabel l3 = new JLabel("员工ID:");
 
-		JLabel l4 = new JLabel("姓名:");
-		JLabel l5 = new JLabel("职位:");
-		JLabel l6 = new JLabel("年龄:");
-		JLabel l7 = new JLabel("手机:");
-		JLabel l8 = new JLabel("薪水:");
-		JLabel l9 = new JLabel("身份证:");
-		JLabel l10 = new JLabel("地址:");
-		JLabel l11 = new JLabel("性别:");
-		JLabel l12 = new JLabel("地区:");
-		JLabel posIDlabel = new JLabel("机构编号:");
-		l3.setFont(new Font("", Font.PLAIN, b2size));
-		l4.setFont(new Font("", Font.PLAIN, b2size));
-		l5.setFont(new Font("", Font.PLAIN, b2size));
-		l6.setFont(new Font("", Font.PLAIN, b2size));
-		l7.setFont(new Font("", Font.PLAIN, b2size));
-		l8.setFont(new Font("", Font.PLAIN, b2size));
-		l9.setFont(new Font("", Font.PLAIN, b2size));
-		l10.setFont(new Font("", Font.PLAIN, b2size));
-		l11.setFont(new Font("", Font.PLAIN, b2size));
-		l12.setFont(new Font("", Font.PLAIN, b2size));
-		posIDlabel.setFont(new Font("", Font.PLAIN, b2size));
+		p1.setBounds(0, 0, 1029, 840);
+
 		final JTextField id = new JTextField();
 		final JTextField name = new JTextField();
 		final JTextField age = new JTextField();
@@ -95,10 +67,9 @@ public class deleteemployee {
 		final JTextField place = new JTextField();
 		final JTextField sex = new JTextField();
 		final JTextField posidtext = new JTextField();
-		
+
 		id.setOpaque(false);
 		id.setBorder(BorderFactory.createEmptyBorder());
-		
 		name.setOpaque(false);
 		name.setBorder(BorderFactory.createEmptyBorder());
 		name.setEditable(false);
@@ -129,19 +100,16 @@ public class deleteemployee {
 		posidtext.setOpaque(false);
 		posidtext.setBorder(BorderFactory.createEmptyBorder());
 		posidtext.setEditable(false);
-		
-	
 
-		JButton b4 = new JButton("查询ID");
+		JButton b4 = new JButton();
 		b4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EmploeeBl jk = new EmploeeBl(oos,ois);
+				EmploeeBl jk = new EmploeeBl(oos, ois);
 				EmploeePO empo = jk.IDGetEmp(id.getText());
-				
-				if(empo.getPosition().equals("不存在")){
+
+				if (empo.getPosition().equals("不存在")) {
 					JOptionPane.showMessageDialog(null, "没有该员工ID!");
-		     	}
-				else{
+				} else {
 					name.setText(empo.getName());
 					place.setText(empo.getArea());
 					age.setText(String.valueOf(empo.getAge()));
@@ -157,14 +125,14 @@ public class deleteemployee {
 
 		});
 
-		JButton b5 = new JButton("确认删除");
+		JButton b5 = new JButton();
 		b5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			
+
 				boolean idisempty = id.getText().equals("");
 				boolean nameisempty = name.getText().equals("");
 				boolean isempty = idisempty || nameisempty;
-				EmploeeBl ac = new EmploeeBl(oos,ois);
+				EmploeeBl ac = new EmploeeBl(oos, ois);
 				boolean isOk = ac.DeleteEmp(id.getText());
 				if (!isempty && isOk)
 					JOptionPane.showMessageDialog(null, "删除成功!");
@@ -177,19 +145,6 @@ public class deleteemployee {
 
 		p1.setOpaque(false);
 		p1.setLayout(null);
-		p1.add(l1);
-		p1.add(l2);
-		p1.add(l3);
-		p1.add(l4);
-		p1.add(l5);
-		p1.add(l6);
-		p1.add(l7);
-		p1.add(l8);
-		p1.add(l9);
-		p1.add(l10);
-		p1.add(l11);
-		p1.add(l12);
-		p1.add(posIDlabel);
 
 		p1.add(id);
 		p1.add(name);
@@ -206,45 +161,26 @@ public class deleteemployee {
 		p1.add(b4);
 		p1.add(b5);
 
-		int b1xloc = p1.getWidth() * 7 / 12 + 20, b1xsize = p1.getWidth() * 4 / 25 - 15;
-		int b1yloc = p1.getHeight() * 2 / 15;
-		int b4xloc = p1.getWidth() * 2 / 5;
-		int b4yloc = 620, b4ysize = p1.getHeight() * 1 / 5 + 10;
-		int labelw = 150;
-		int labelh = 30;
-		l1.setBounds(220, -20, 180, 80);
+		int textw = 140;
+		int texth = 29;
+		id.setBounds(182, 237, textw, texth);
+		name.setBounds(644, 167, 94, 27);
+		job.setBounds(182, 467, textw, texth);
+		age.setBounds(300, 350, 94, 27);
+		tel.setBounds(644, 237, 279, 30);
+		salary.setBounds(182, 555, textw, texth);
+		identity.setBounds(644, 389, 279, 30);
+		address.setBounds(644, 463, 279, 30);
+		sex.setBounds(646, 555, 94, 27);
+		place.setBounds(182, 310, textw, texth);
+		posidtext.setBounds(182, 386, textw, texth);
 
-		l2.setBounds(50, 50, labelw, labelh);
-
-		l3.setBounds(150, 100, labelw, labelh);
-
-		l4.setBounds(150, 150, labelw, labelh);
-		l5.setBounds(150, 300, labelw, labelh);
-		l6.setBounds(150, 350, labelw, labelh);
-		l7.setBounds(150, 400, labelw, labelh);
-		l8.setBounds(150, 450, labelw, labelh);
-		l9.setBounds(150, 500, labelw, labelh);
-		l10.setBounds(150, 550, labelw, labelh);
-		l11.setBounds(150, 600, labelw, labelh);
-		l12.setBounds(150, 200, labelw, labelh);
-		posIDlabel.setBounds(150, 250, labelw, labelh);
-		int textw = 150;
-		int texth = 30;
-		id.setBounds(300, 100, textw, texth);
-		name.setBounds(300, 150, textw, texth);
-		job.setBounds(300, 300, textw, texth);
-		age.setBounds(300, 350, textw, texth);
-		tel.setBounds(300, 400, textw, texth);
-		salary.setBounds(300, 450, textw, texth);
-		identity.setBounds(300, 500, textw, texth);
-		address.setBounds(300, 550, textw, texth);
-		sex.setBounds(300, 600, textw, texth);
-		place.setBounds(300, 200, textw, texth);
-		posidtext.setBounds(300, 250, textw, texth);
-
-		b4.setBounds(150 + labelw + textw + 30, 100, 150, 30);
-
-		b5.setBounds(300, b4yloc, 150, 30);
+		b4.setBounds(398, 235, 88, 34);
+		b5.setBounds(410, 697, 283, 63);
+		b4.setContentAreaFilled(false);
+		b4.setBorder(BorderFactory.createEmptyBorder());
+		b5.setContentAreaFilled(false);
+		b5.setBorder(BorderFactory.createEmptyBorder());
 		return p1;
 
 	}
